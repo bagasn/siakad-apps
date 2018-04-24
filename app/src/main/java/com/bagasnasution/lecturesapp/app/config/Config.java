7ad8199effda125a9cc4579862eaabb497a42b58
bagasndeveloper@gmail.com <bagasnasution@gmail.com>
2018-04-24 07:17:26 +0700

    iseng doang

--- app/src/main/java/com/bagasnasution/lecturesapp/app/config/Config.java
+++ app/src/main/java/com/bagasnasution/lecturesapp/app/config/Config.java
@@ -10,12 +10,18 @@ public class Config {
     public static final String DATABASE_NAME = "lecturesapp_db_master_lectapp.db";
     public static final int DATABASE_VERSION = 4;
 
-    /* Backend Declaration */
+    /** Backend Declaration */
+
     /* Real */
 //    public static final String BASE_URL = "https://itandcrime.000webhostapp.com/api/";
 
     /* Dummy */
+
+//  office
+//    public static final String BASE_URL = "http://10.0.2.2:8080/b/demo_app/ta-api/";
+//  home
     public static final String BASE_URL = "http://10.0.2.2:8080/b/demo_app/ta-api/";
+
 
     public static final String API_TOKEN = "3add0f3378dc0fba4bbf2fab59a312cc";
 
